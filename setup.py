d65afd841cde3446060574931a1677b6918954b5
Aditya_Thakare <adityathakare72@gmail.com>
2024-12-23 03:19:37 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='just testing out versioning using dvc and git',
+    author='adi',
+    license='',
+)
